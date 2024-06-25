57f2d723d44b041c4a29cf927dae3bb0e6141a7f
Katsuba Igor <kasubaigor@gmail.com>
2024-06-25 17:24:49 +0300

    ДЗ5

---
+++ tests/conftest.py
@@ -0,0 +1,15 @@
+from selene import browser
+
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture(autouse=True)
+def browser_management():
+    driver_options = webdriver.ChromeOptions()
+    driver_options.add_argument('--headless')
+    browser.config.driver_options = driver_options
+    browser.config.window_width = 1200
+    browser.config.window_height = 1200
+    yield
+    browser.quit()
---
+++ tests/test_toolsqa_practice_form.py
@@ -0,0 +1,39 @@
+from selene import browser, have
+import os
+
+
+def test_student_registration_form():
+    browser.open('https://demoqa.com/automation-practice-form')
+    browser.should(have.title('DEMOQA'))
+    browser.element('#firstName').set_value('Ivan')
+    browser.element('#firstName').should(have.value('Ivan'))
+    browser.element('#lastName').set_value('Ivanov')
+    browser.element('#lastName').should(have.value('Ivanov'))
+    browser.element('#userEmail').set_value('IvanIvanov@test.com')
+    browser.element('#userEmail').should(have.value('IvanIvanov@test.com'))
+    browser.element('[for="gender-radio-3"]').click()
+    browser.element('#userNumber').set_value('1616161616')
+    browser.element('#userNumber').should(have.value('1616161616'))
+    browser.element('[class="react-datepicker__input-container"]').click()
+    browser.element('[class="react-datepicker__month-select"]').element('[value="5"]').click()
+    browser.element('[class="react-datepicker__year-select"]').element('[value="2007"]').click()
+    browser.element('.react-datepicker__month').element('[aria-label="Choose Saturday, June 16th, 2007"]').click()
+    browser.element('#subjectsInput').type('Che').press_enter()
+    browser.element('[for="hobbies-checkbox-1"]').click()
+    browser.element('[for="hobbies-checkbox-3"]').click()
+    browser.element('#uploadPicture').send_keys(os.path.abspath('pic.jpg'))
+    browser.element("#currentAddress").type("Kyoto, Pushkin Street, 16")
+    browser.element("#react-select-3-input").type("Haryana").press_enter()
+    browser.element("#react-select-4-input").type("Karnal").press_enter()
+    browser.element("#submit").click()
+    browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
+    browser.element('.table').all('td').even.should(have.exact_texts('Ivan Ivanov',
+                                                                     'IvanIvanov@test.com',
+                                                                     'Other',
+                                                                     '1616161616',
+                                                                     '25 June,2024',
+                                                                     'Social Studies',
+                                                                     'Sports, Music',
+                                                                     'pic.png',
+                                                                     'Kyoto, Pushkin Street, 16',
+                                                                     'Haryana Karnal'))
