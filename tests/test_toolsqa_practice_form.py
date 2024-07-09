f26eb3ad9fe05f4e8c76ac38b61108b0cd12b013
Katsuba Igor <kasubaigor@gmail.com>
2024-07-09 17:33:42 +0300

    иcправления mid

--- tests/test_toolsqa_practice_form.py
+++ tests/test_toolsqa_practice_form.py
@@ -1,8 +1,8 @@
-from modules.registration_from import Registration_form
+from modules.registration_from import RegistrationForm
 
 
 def test_student_registration_form():
-    form_action = Registration_form()
+    form_action = RegistrationForm()
     form_action.open('automation-practice-form')
     form_action.first_name('Ivan')
     form_action.last_name('Ivanov')
@@ -12,10 +12,11 @@ def test_student_registration_form():
     form_action.date_of_birth()
     form_action.subjects('Che')
     form_action.hobbies()
-    form_action.upload_picture()
+    form_action.upload_picture('pic.jpg')
     form_action.current_address("Kyoto, Pushkin Street, 16")
     form_action.state('Haryana')
     form_action.city('Karnal')
     form_action.sumbit_button()
-    form_action.check_text_after_submit_button()
-    form_action.check_table()
+    form_action.check_text_after_submit_button('Thanks for submitting the form')
+    form_action.check_table('Ivan Ivanov', 'Vasya_the_terrible_2005@mail.ru', 'Other', '1987198719', '16 June,2007', 'Chemistry',
+                             'Sports, Music', 'pic.jpg', 'Kyoto, Pushkin Street, 16', 'Haryana Karnal')
